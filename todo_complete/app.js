90235e8117b35ff15998d21f85c843700fc73387
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-04-03 14:33:21 -0400

    added todo app example

---
+++ todo_complete/app.js
@@ -0,0 +1,41 @@
+// include our Mongoose setup
+require('./db');
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var engine = require('ejs-locals');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.engine( 'ejs', engine );
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Routes
+app.get('/', routes.index);
+app.post('/create', routes.create);
+app.get('/destroy/:id', routes.delete);
+app.get('/edit/:id', routes.edit);
+app.post( '/update/:id', routes.update );
+
+// Run our Node server
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
