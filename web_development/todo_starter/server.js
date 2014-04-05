d03210c9c226b753401ab1cc1bff089185739060
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-04-05 07:58:49 -0400

    updated package.json and server.js

--- web_development/todo_starter/server.js
+++ web_development/todo_starter/server.js
@@ -2,11 +2,18 @@
 require('./models/todo');
 
 var express = require('express');
+var home = require('./controllers/home');
 var todos = require('./controllers/todos');
 var http = require('http');
 var path = require('path');
 var engine = require('ejs-locals');
 var app = express();
+var mongoose = require('mongoose');
+
+// connect to the database
+var uristring = process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL || 'mongodb://localhost/express-todo';
+mongoose.connect(uristring);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -27,7 +34,12 @@ if ('development' === app.get('env')) {
 }
 
 // Routes
-
+app.get('/', home.index);
+app.get('/todos', todos.index);
+app.post('/todos/create', todos.create);
+app.get('/todos/destroy/:id', todos.delete);
+app.get('/todos/edit/:id', todos.edit);
+app.post('/todos/update/:id', todos.update);
 
 // Run our Node server
 var port = app.get('port');
